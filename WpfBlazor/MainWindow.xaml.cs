be55896bf3cc0a298286769f4a589c0552b717a2
kaworu20220420 <104045473+kaworu20220420@users.noreply.github.com>
2025-04-09 14:45:31 +0900

    .NET9対応

--- WpfBlazor/MainWindow.xaml.cs
+++ WpfBlazor/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Components.WebView.Wpf;
 
 namespace WpfBlazor
 {
